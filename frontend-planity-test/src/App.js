b56dedd05f539d2f2a02e8862a44fd775913b6c0
Mehdy <chelmehdy@gmail.com>
2021-05-14 23:08:01 +0200

    initialize front-end test

---
+++ frontend-planity-test/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import planity from "./planity.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={planity} alt="logo" className="App-logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
